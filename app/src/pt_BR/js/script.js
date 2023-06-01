3cbfd2f5b913c8f01397596cee5b4d853febec9d
Rafael Forato <rafaelforato5@gmail.com>
2023-06-01 18:00:38 -0300

    add new sections and features

--- app/src/pt_BR/js/script.js
+++ app/src/pt_BR/js/script.js
@@ -1,4 +1,12 @@
 function toggleMode() {
     const body = document.querySelector('body')
+    let githubIcon = document.querySelector('.github-icon img')
+    const bodyDark = body.classList.contains('dark')
     body.classList.toggle('dark')
+    
+    if(bodyDark === false) {
+        githubIcon.setAttribute('src', '/app/assets/icons8-github-white.svg')
+    } else {
+        githubIcon.setAttribute('src', '/app/assets/icons8-github.svg')
+    }
 }
