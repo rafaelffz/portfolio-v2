5f802914a6bb7b3c8b754041e9d166780a4b0331
Rafael Forato <rafaelforato5@gmail.com>
2023-06-17 18:11:01 -0300

    add media queries and take out english language

---
+++ app/src/js/script.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+    const body = document.querySelector('body')
+    let githubIcon = document.querySelector('.github-icon img')
+    const bodyDark = body.classList.contains('dark')
+    body.classList.toggle('dark')
+    
+    if(bodyDark === false) {
+        githubIcon.setAttribute('src', './assets/icons/icons8-github-white.svg')
+    } else {
+        githubIcon.setAttribute('src', './assets/icons/icons8-github.svg')
+    }
+}
+
+const btnMobile = document.getElementById('btn-mobile')
+
+function toggleMenu(event) {
+    if(event.type === 'touchstart') event.preventDefault()
+    const nav = document.querySelector('nav ul')
+    nav.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
+
+function closeMenu() {
+    btnMobile.click()
+}
